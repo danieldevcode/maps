c8fb2e5b640938673a3601c18f19753d008307b1
Daniel <danieldevcode@gmail.com>
2022-04-29 02:30:01 -0500

    final

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ console.log("Load Ok")
 const myMap = L.map('map').setView([0, 0], 1);
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright"> OpenStreetMap </a> contributors'
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-const tiles = L.tileLayer(tileUrl, {attribution})
+const tiles = L.tileLayer(tileUrl, { attribution })
 const marker = L.marker([0, 0]).addTo(myMap)
 
 tiles.addTo(myMap)
@@ -13,18 +13,19 @@ let options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
-  };
-  
-  navigator.geolocation.getCurrentPosition(success, error, options)
-  
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options)
+
 function success(pos) {
     let coordinates = pos.coords
-    
+
     consoleReport(coordinates)
 
     // Render Current location
     document.getElementById("latitud").textContent += `${coordinates.latitude}`
     document.getElementById("longitud").textContent += `${coordinates.longitude}`
+    document.getElementById("coordenada").textContent += `${coordinates.latitude},${coordinates.longitude}`
 
     // Marker in map
     marker.setLatLng([coordinates.latitude, coordinates.longitude])
@@ -35,14 +36,14 @@ function success(pos) {
         "longitude": coordinates.longitude
     }
 }
-  
+
 function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`)
 }
 
-function coordinateSearch(){
-    let latitude= document.getElementById("latitudInput").value
-    let longitude= document.getElementById("longitudInput").value
+function coordinateSearch() {
+    let latitude = document.getElementById("latitudInput").value
+    let longitude = document.getElementById("longitudInput").value
     let coordinates = {
         "latitude": latitude,
         "longitude": longitude,
@@ -59,3 +60,22 @@ function consoleReport(coordinates) {
             Longitud: ${coordinates.longitude}
                 Aproximadamente ${coordinates.accuracy} metros.`)
 }
+
+let geocoder = L.Control.Geocoder.nominatim();
+if (typeof URLSearchParams !== 'undefined' && location.search) {
+    var params = new URLSearchParams(location.search);
+    var geocoderString = params.get('geocoder');
+    if (geocoderString && L.Control.Geocoder[geocoderString]) {
+        console.log('Geocoder en uso', geocoderString);
+        geocoder = L.Control.Geocoder[geocoderString]();
+    } else if (geocoderString) {
+        console.warn('Geocoder no soportado', geocoderString);
+    }
+}
+
+var control = L.Control.geocoder({
+    query: 'Colima México',
+    placeholder: 'Buscar aquí...',
+    geocoder: geocoder
+}).addTo(myMap);
+
