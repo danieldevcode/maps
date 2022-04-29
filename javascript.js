35671986e9ffe231811790795d71bb33d5e46390
Daniel <danieldevcode@gmail.com>
2022-04-28 21:20:44 -0500

    coordinate

--- javascript.js
+++ javascript.js
@@ -1,9 +1,21 @@
-function success(position) {
-    let coordinates = position.coords
-    console.log(coordinates)
-}
-
-function error(error) {
-    console.warn(`Error(${error.code}):(${error.message})`)
-}
-navigator.geolocation.getCurrentPosition(success, error)
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+  
