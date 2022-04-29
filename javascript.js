182835c337064599f85e2397256daec7cd1ef901
Daniel <danieldevcode@gmail.com>
2022-04-28 23:16:59 -0500

    coordinates searching

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-console.log("Ok")
+console.log("Load Ok")
+
+// Map Latitud, Longitud, Zoom
+const myMap = L.map('map').setView([0, 0], 1);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright"> OpenStreetMap </a> contributors'
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const tiles = L.tileLayer(tileUrl, {attribution})
+const marker = L.marker([0, 0]).addTo(myMap)
+
+tiles.addTo(myMap)
 
 let options = {
     enableHighAccuracy: true,
@@ -6,17 +15,47 @@ let options = {
     maximumAge: 0
   };
   
-  function success(pos) {
+  navigator.geolocation.getCurrentPosition(success, error, options)
+  
+function success(pos) {
     let coordinates = pos.coords
+    
+    consoleReport(coordinates)
+
+    // Render Current location
+    document.getElementById("latitud").textContent += `${coordinates.latitude}`
+    document.getElementById("longitud").textContent += `${coordinates.longitude}`
+
+    // Marker in map
+    marker.setLatLng([coordinates.latitude, coordinates.longitude])
+
+    // Coordinates object
+    let coordinatesObj = {
+        "latitude": coordinates.latitude,
+        "longitude": coordinates.longitude
+    }
+}
   
-    console.log('Tu posición actual es:')
-    console.log(`Latitud : ${coordinates.latitude}`)
-    console.log(`Longitud: ${coordinates.longitude}`)
-    console.log(`Aproximadamente ${coordinates.accuracy} metros.`)
-  }
-  
-  function error(err) {
+function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`)
-  }
-  
-navigator.geolocation.getCurrentPosition(success, error, options)
+}
+
+function coordinateSearch(){
+    let latitude= document.getElementById("latitudInput").value
+    let longitude= document.getElementById("longitudInput").value
+    let coordinates = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "accuracy": ""
+    }
+    consoleReport(coordinates)
+    marker.setLatLng([latitude, longitude])
+}
+
+function consoleReport(coordinates) {
+    console.log(`
+        Tu posicion actual es:
+            Latitud : ${coordinates.latitude}
+            Longitud: ${coordinates.longitude}
+                Aproximadamente ${coordinates.accuracy} metros.`)
+}
